ed30744028294e7dd40f03d6454a3bdab4d43c95
Marko Golub <marko.golub@fer.hr>
2020-07-07 21:00:36 +0200

    Initialization

---
+++ Django/StorageSystemAPI/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/golub/5ee0dd7e-0856-4e9f-82f8-59ca97092b2f/home/golub/Documents/Projects/REST_API/Django/StorageSystemAPI/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
