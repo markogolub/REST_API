58ff98c840004c3af618c03de460b8439f311f2b
Marko Golub <marko.golub@fer.hr>
2020-07-09 20:07:11 +0200

    Added login

--- Django/StorageSystemAPI/src/mysite/urls.py
+++ Django/StorageSystemAPI/src/mysite/urls.py
@@ -20,11 +20,13 @@ from personal.views import home_screen_view
 from account.views import (
     registation_view,
     logout_view,
+    login_view,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
     path('register/', registation_view, name="register"),
-    path('logout/', logout_view, name="logout")
+    path('logout/', logout_view, name="logout"),
+    path('login/', login_view, name="login")
 ]
