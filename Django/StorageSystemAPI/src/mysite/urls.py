fa0f2e92326c98634576f51b4c72f16e54f5f555
Marko Golub <marko.golub@fer.hr>
2020-07-09 19:25:18 +0200

    Made authentication more visable, added link to register page and enabled logout

--- Django/StorageSystemAPI/src/mysite/urls.py
+++ Django/StorageSystemAPI/src/mysite/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from personal.views import home_screen_view
-from account.views import registation_view
+
+from account.views import (
+    registation_view,
+    logout_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name="home"),
-    path('register/', registation_view, name="register")
+    path('register/', registation_view, name="register"),
+    path('logout/', logout_view, name="logout")
 ]
